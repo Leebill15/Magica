12a0b6fdc39656e0673919540deebca9a914aaa5
Hermann <slot2@herobrine.zocker.org>
2020-03-16 21:25:38 +0100

    First commit Message

---
+++ sponge.sh
@@ -0,0 +1,2 @@
+#!/bin/bash 
+java -Xms2048M -Xmx8096M -jar spongeforge-1.12.2-2838-7.1.10.jar nogui
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash 
+java -Xms4048M -Xmx10096M -jar forge-1.12.2-14.23.5.2847-universal.jar nogui
